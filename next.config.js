efaaedbd32db4c28c52f0496db7d99704b9236d4
HirofumiHarada <hirofumi.harada@writech.co.jp>
2024-10-30 20:31:46 +0900

    Update next.config.js to specify output directory

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     domains: ['dl.airtable.com'], // 後でAirtableの画像を使用する際に必要
   },
+  distDir: '.next', // 出力ディレクトリを明示的に指定
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
